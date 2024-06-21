4fd41abffad1c955455edc9e9d5a87c484a61057
PhanTrongHau <haupk2003@gmail.com>
2024-06-21 16:23:14 +0700

    add home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,50 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [email, setEmail] = useState("");
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const handleButtonStart = (e) => {
+    if (validateEmail(email)) {
+      alert("Email hợp lệ");
+    } else {
+      alert("Email không hợp lệ");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setEmail(e.target.value);
+  };
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <main>
+      <section>
+        <h1>
+          Công ty bạn trưởng thành như thế nào trong việc lắng nghe khách hàng
+        </h1>
+        <p>
+          Đánh giá khả năng của bạn trong việc lắng nghe, hiểu và đáp ứng các
+          tín hiệu từ khách hàng
+        </p>
+        <label>
+          <input
+            type="text"
+            placeholder="Địa chỉ email của bạn"
+            onChange={handleOnChange}
+          />
+          <button type="submit" onClick={handleButtonStart}>
+            Bắt đầu
+          </button>
+        </label>
+      </section>
+    </main>
   );
 };
 
